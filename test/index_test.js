2c9ac9b13e54309414a1c11ac4b0d8aa4896a921
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-13 14:34:02 -0400

    create a describe for '.timeAtDawn'

--- test/index_test.js
+++ test/index_test.js
@@ -5,15 +5,51 @@ const Rooster = require('../JS/index.js');
 describe('Rooster', () => {
     describe('.announceDawn', () => {
         it('return a rooster call', () => {
-  
+
           // Setup
           const expected = 'cock-a-doodle-doo!';
-  
+
           // Exercise
           const actual = Rooster.announceDawn();
-  
+
           // Verify
           assert.equal(actual, expected);
+        });
+    });
+    describe('.timeAtDawn', () => {
+        it('returns its argument as a string', () => {
+
+          // Setup
+          const inputNumber = 12;
+          const expected = '12';
+
+          // Exercise
+          const actual = Rooster.timeAtDawn(inputNumber);
+
+          // Verify
+          assert.strictEqual(actual, expected);
+        });
+        it('thows a range error if passed a number less than 0', () => {
+
+          // Setup
+          const inputNumber = -1;
+          const expected = RangeError;
+
+          // Verify
+          assert.throws(() => {
+            Rooster.timeAtDawn(inputNumber); // Exercise
+          }, expected);
+        });
+        it('thows a range error if passed a number greater than 23', () => {
+
+          // Setup
+          const inputNumber = 24;
+          const expected = RangeError;
+
+          // Verify
+          assert.throws(() => {
+            Rooster.timeAtDawn(inputNumber); // Exercise
+          }, expected);
         });
     });
 });
