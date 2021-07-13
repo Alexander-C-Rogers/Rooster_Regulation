2ab4e0ced91647c0cc838800fdc32713f696b55a
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-13 14:31:19 -0400

    create a describe for '.announceDawn'

--- test/index_test.js
+++ test/index_test.js
@@ -2,4 +2,18 @@
 const assert = require('assert');
 const Rooster = require('../JS/index.js');
 
-describe('Rooster', () => {});
+describe('Rooster', () => {
+    describe('.announceDawn', () => {
+        it('return a rooster call', () => {
+  
+          // Setup
+          const expected = 'cock-a-doodle-doo!';
+  
+          // Exercise
+          const actual = Rooster.announceDawn();
+  
+          // Verify
+          assert.equal(actual, expected);
+        });
+    });
+});
